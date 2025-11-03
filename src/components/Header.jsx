7b915579daef4215c2d45476daa4e379f675b11c
Matias Jørgensen <matjorrub@gmail.com>
2025-11-03 14:12:28 +0100

    first folder structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <header>
+            <h1>{"<Matias Jørgensen />"}</h1>
+            <ul>
+                <li>Om</li>
+                <li>Projekter</li>
+                <li>Kontakt</li>
+            </ul>
+        </header>
+    )
+}
