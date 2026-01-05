ab5d7471d4b619e8bf648df6fd4779b3e8c5d4d3
Matias Jørgensen <matjorrub@gmail.com>
2026-01-05 09:08:59 +0100

    Changeg SVG icons for various technologies from asstes to public folder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,3 @@
-import { FaReact, FaHtml5, FaCss3Alt, FaSass, FaJs } from "react-icons/fa";
-
 export default function Hero() {
   return (
     <section className="hero">
@@ -7,7 +5,7 @@ export default function Hero() {
         <h1>Matias Jørgensen</h1>
         <p>Frontend Developer Student</p>
         <ul className="hero__skills">
-          <li><img src="C:\VScode\Hovedforløb\Frameworks\Portfolio V2\src\assets\img\icons\html5.svg" alt="html" /></li>
+          <li><img src=".../icons/html5.svg" alt="html" /></li>
         </ul>
         <ul className="hero__nav">
           <li>
